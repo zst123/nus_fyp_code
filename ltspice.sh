2067610c5cd56297bf5cc0b1032392ab8a72c019
zst123 <zst123@users.noreply.github.com>
2022-09-30 23:35:28 +0800

    Presistent wineserver

--- ltspice.sh
+++ ltspice.sh
@@ -1,3 +1,4 @@
 export DISPLAY=:0.0
 # xhost +local:docker
+docker exec "ltspice" wineserver --persistent
 docker exec "ltspice" wine "/home/appuser/.wine/drive_c/Program Files/LTC/LTspiceXVII/XVIIx86.exe" $@
